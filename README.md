9a097a9795bdc9eccb0273f6c7cc840cf2091b24
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-01-04 09:34:08 +0800

    Amend text

--- README.md
+++ README.md
@@ -155,8 +155,12 @@ export default ConditionalRendering;
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <h1>Counter</h1>
--- react/src/components/counter/Counter.tsx
+++ react/src/components/counter/Counter.tsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const increment = () => {
+    setCount(() => count + 1)
+  };
+  const decrement = () => {
+    setCount(() => count - 1)
+  };
   return (
     <div>
       <h1>Counter</h1>
